0a912b28a9ee78fe4161637b0965855ffdce46ab
garvsl <garvsehgal7@gmail.com>
2023-01-28 20:21:46 -0500

    add button for test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    changeDisplay(".butt");
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display counter={counter} />
+      <button className="butt" onClick={handleClick} />
     </div>
   );
+}
+
+function Display(props: { counter: number }) {
+  return <h1>Server Test {props.counter} </h1>;
+}
+
+function changeDisplay(props: string) {
+  console.log("hi");
+  (document.querySelector(props) as HTMLElement).style.backgroundColor =
+    "rgb(173, 255, 169)";
+  (document.querySelector(props) as HTMLElement).style.opacity = "0.6";
+  setTimeout(() => {
+    (document.querySelector(props) as HTMLElement).style.opacity = "1";
+  }, 250);
+  (document.querySelector(props) as HTMLElement).style.borderColor = "black";
 }
 
 export default App;
