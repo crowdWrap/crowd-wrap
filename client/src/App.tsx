670b40217183f0e0007f167626b4816d0b46d371
garvsl <garvsehgal7@gmail.com>
2023-01-31 13:50:12 -0500

    cleanup file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,35 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import Engineers from "./components/Engineers";
+
+function changeDisplay(props: string) {
+  (document.querySelector(props) as HTMLElement).classList.add(
+    "startBtnActivated"
+  );
+  (document.querySelector(props) as HTMLElement).classList.toggle(
+    "startBtnBuffer"
+  );
+  setTimeout(() => {
+    (document.querySelector(props) as HTMLElement).classList.toggle(
+      "startBtnBuffer"
+    );
+  }, 250);
+}
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [engineers, setEngineers] = useState([]);
-  const [showEngineers, setShowEngineers] = useState(false);
+  const [counter, setCounter] = useState<number>(0);
+  const [allEngineers, setAllEngineers] = useState([]);
+  const [showEngineers, setShowEngineers] = useState<Boolean>(false);
 
   const handleClick = async () => {
-    changeDisplay(".butt");
+    changeDisplay(".startBtn");
     setCounter(counter + 1);
     setShowEngineers(true);
 
     try {
       const response = await fetch("/crowdWrap/engineers");
       const json = await response.json();
-      setEngineers(json);
+      setAllEngineers(json);
     } catch (error) {
       console.log(error);
     }
@@ -24,37 +37,9 @@ function App() {
 
   return (
     <div className="App">
-      <Display counter={counter} />
-      <button className="butt" onClick={handleClick} />
-      {showEngineers && <Engineers engineers={engineers} />}
-    </div>
-  );
-}
-
-function Display(props: { counter: number }) {
-  return <h1>Server Test {props.counter} </h1>;
-}
-
-function changeDisplay(props: string) {
-  (document.querySelector(props) as HTMLElement).style.backgroundColor =
-    "rgb(173, 255, 169)";
-  (document.querySelector(props) as HTMLElement).style.opacity = "0.6";
-  setTimeout(() => {
-    (document.querySelector(props) as HTMLElement).style.opacity = "1";
-  }, 250);
-  (document.querySelector(props) as HTMLElement).style.borderColor = "black";
-}
-
-//run server and react
-
-function Engineers({ engineers }: { engineers: Array<{ engineer: any }> }) {
-  return (
-    <div className="engineer">
-      <ul>
-        {engineers.map((e) => {
-          return <li key={e.engineer}> {e.engineer} </li>;
-        })}
-      </ul>
+      <h1>Server Test {counter} </h1>
+      <button className="startBtn" onClick={handleClick} />
+      {showEngineers && <Engineers engineers={allEngineers} />}
     </div>
   );
 }
