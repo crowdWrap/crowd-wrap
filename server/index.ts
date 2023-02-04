cbda8fe50b799c08bee2836b517d24b993879107
Vinh Pham <viph0009@gmail.com>
2023-02-03 20:58:35 -0800

    Edited server/package.json for cleaner scripts' names and deleted code used in testing in server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -5,8 +5,4 @@ const port = 8000;
 
 app.listen(port, () => {
   console.log(`Server started: http://localhost:8000}/`);
-  var word = "Bigups CrowdWrap";
-  console.log(word);
-  //concurrently 'run tsc_watch' 'npm run nm_disc_src'
-  //tsc -w & nodemon ./dist/index.js
 });
