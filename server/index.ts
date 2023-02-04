ea98ba2994b557f6009bb7176f5d1001f0541f1d
Vinh Pham <viph0009@gmail.com>
2023-02-03 20:42:01 -0800

    downloaded Concurrently and placed commands into separate scripts

--- server/index.ts
+++ server/index.ts
@@ -5,4 +5,8 @@ const port = 8000;
 
 app.listen(port, () => {
   console.log(`Server started: http://localhost:8000}/`);
+  var word = "Bigups CrowdWrap";
+  console.log(word);
+  //concurrently 'run tsc_watch' 'npm run nm_disc_src'
+  //tsc -w & nodemon ./dist/index.js
 });
