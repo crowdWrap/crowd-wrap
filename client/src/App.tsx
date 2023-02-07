14d41fda34ccb51c0a8998ef34929cefc7b20e00
garvsl <garvsehgal7@gmail.com>
2023-02-06 22:10:45 -0500

    fix merge header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,26 +31,9 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <h1>Server Test {counter} </h1>
       <button className={className} onClick={handleClick} />
       {showEngineers && <Engineers allEngineers={allEngineers} />}
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. Hello World.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> main
     </div>
   );
 }
