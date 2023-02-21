81dae4456a0c8f1cde2ceefca638f84cfbe7d919
VinnyXP <96674324+VinnyXP@users.noreply.github.com>
2023-02-20 17:25:34 -0800

    #9 and #28 - Choose a Database and Connect to Database (#7)
    * add button for test
    
    * add nodemon
    
    * set proxy to 8000
    
    * create function to display engineers
    
    * create router and middleman
    
    * add script to start
    
    * add scripts and fix proxy
    
    * create new file for Engineers.tsx component
    
    * change class names and make new ones
    
    * cleanup file
    
    * change import statements
    
    * change start command
    
    * improve display to screen
    
    * connect text file
    
    * add new component
    
    * change li into component
    
    * simplify component
    
    * correct names
    
    * fix quotes
    
    * setting up postgreSQL db on AWS rds
    
    * fixed some issues with running server code and client code
    
    * Eliminate prev url
    
    ---------
    
    Co-authored-by: garvsl <garvsehgal7@gmail.com>

---
+++ server/prisma/migrations/script.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+          name: 'Bob',
+          email: 'bob@prisma.io',
+        },
+      })
+      console.log(user) 
+
+    const users = await prisma.user.findMany()
+    console.log(users)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
