2198b3327c1d163d49877229c14bb0c6fb83dd8c
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2023-02-24 17:35:33 -0500

    basic query setup and testing Co-authored-by: Garv

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,14 @@
-
 import express from "express";
 import morgan from 'morgan';
 import { router } from "./routes/engineers";
+import getProfile from "./queries"
+
+
 
 const app = express();
 app.use(morgan('combined'));
 
-app.listen(8000, () => {
-  console.log('Server is listening on port 8000');
-});
+
 
 app.use("/crowdWrap/engineers", router);
 
@@ -21,11 +21,19 @@ app.post("/register", (req, res) => {
   res.status(200).json({ message: "Registration successful" });
 })
 
-app.post("/login", (req, res) => {
-  console.log(req.body, "BACKEND");
-  res.status(200).json({ message: "login successful" });
+app.post("/login", async (req, res) => {
+  const profile = await getProfile(res);
+  const response = JSON.stringify(profile);
+  console.log(response);
+  res.status(200).send(response);
 })
 
-app.post("/profile", (req, res) => {})
+app.post("/profile", (req, res) => {
+  
+})
+
+app.listen(8000, () => {
+  console.log('Server is listening on port 8000');
+});
 
 export { app };
