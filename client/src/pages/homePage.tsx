f086c348b7287c7e4a6373526982fc395ecb639a
garvsl <garvsehgal7@gmail.com>
2023-03-02 00:37:48 -0500

    cleanup homepage

--- client/src/pages/homePage.tsx
+++ client/src/pages/homePage.tsx
@@ -4,24 +4,13 @@ import Engineers from "../components/Engineers";
 import LogoutButton from "../components/logout";
 import SignupForm from "./signupForm";
 import LoginForm from "./loginForm";
-import {Link, Router} from 'react-router-dom'
+import { Link, Router } from "react-router-dom";
 
 export default function HomePage() {
   const [counter, setCounter] = useState<number>(0);
   const [allEngineers, setAllEngineers] = useState<string[]>([]);
   const [showEngineers, setShowEngineers] = useState<Boolean>(false);
   const [className, setClassName] = useState<string>("");
-
-  const [isRegOpen, setIsRegOpen] = useState<boolean>(false);
-  const [isLoginOpen, setIsLoginOpen] = useState<boolean>(false);
-
-  const openRegisterPage = () => {
-    setIsRegOpen(true)
-  }
-
-  const openLoginPage = () => {
-    setIsLoginOpen(true)
-  }
 
   useEffect(() => {
     setClassName("startBtn startBtnActivated startBtnBuffer");
@@ -42,31 +31,22 @@ export default function HomePage() {
     } catch (error) {
       console.log(error);
     }
-  };  
+  };
 
   return (
-   
     <div className="App">
       <h1 className="title">Server Test {counter} </h1>
-      {/* <LogoutButton /> */}
       <button className={className} onClick={handleClick} />
       {showEngineers && <Engineers allEngineers={allEngineers} />}
-      
-      {/* <button onClick={openRegisterPage} className="signupBtn">SignUp</button>;
-      {isRegOpen && <SignupForm/>}
 
-      <button onClick={openLoginPage} className="loginBtn">Login</button>;
-      {isLoginOpen && <LoginForm/>} */}
-      
       <div className="btnWrap">
-      <Link to="/register"><button className="signupBtn"> Sign up</button></Link>
-      <Link to="/login"><button className="loginBtn"> Login</button></Link> 
+        <Link to="/register">
+          <button className="signupBtn"> Sign up</button>
+        </Link>
+        <Link to="/login">
+          <button className="loginBtn"> Login</button>
+        </Link>
       </div>
     </div>
-   
   );
 }
-
-
-
-//React-router-dom, create new page so it links to it.
