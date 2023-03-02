996009149885785db5ae4f5ffb31da145396d6f7
garvsl <garvsehgal7@gmail.com>
2023-03-02 00:34:14 -0500

    have popup remove once doc is clicked

--- client/src/components/logout.tsx
+++ client/src/components/logout.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import ClickPopup from "./logoutPop";
 
-
 export default function LogoutButton() {
   const [clicked, setClicked] = useState<boolean>(false);
   const click = () => {
-    clicked === false ? setClicked(true) : setClicked(false);
+    setClicked(clicked == false);
+    document.addEventListener("click", (e: MouseEvent) => {
+      const target = e.target as Element;
+      if (target && !target.closest(".logoutBtnCover")) {
+        setClicked(false);
+      }
+    });
   };
   return (
     <div className="logoutBtnCover">
       <button
-        className={!clicked ? "logoutBtn" : "logoutBtnClicked"}
+        className={!clicked ? "logoutBtn" : "logoutBtn logoutBtnClicked"}
         onClick={click}
       ></button>
       <div>{clicked && <ClickPopup />}</div>
