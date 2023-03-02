e9fd588d5172047d4bac6c0b0184c43d34dd86cb
garvsl <garvsehgal7@gmail.com>
2023-03-02 00:35:33 -0500

    fix css for logoutpop and incorporate fontawesome

--- client/src/components/logoutPop.tsx
+++ client/src/components/logoutPop.tsx
@@ -1,43 +1,43 @@
-import {Link, useNavigate} from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGear,
+  faUser,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function ClickPopup() {
   const navigate = useNavigate();
   const logoutSession = async () => {
-    const response: Response = await fetch('/logout', { method: 'get'})
-      const receivedData = await response.json();
+    const response: Response = await fetch("/logout", { method: "get" });
+    const receivedData = await response.json();
 
-      console.log(receivedData)
+    console.log(receivedData);
 
-      if (response.ok) {
-         navigate('/login');
-       } else {
-         alert(receivedData.message);
-       }
-  }
+    if (response.ok) {
+      navigate("/login");
+    } else {
+      alert(receivedData.message);
+    }
+  };
 
   return (
     <div className="logoutPop">
       <ul>
         <li>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <title>cog</title>
-            <path d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
-          </svg>
-          Settings
+          <FontAwesomeIcon icon={faGear} />
+          <button className="individualBtn firstIndiv">Settings</button>
         </li>
         <li>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <title>account-circle</title>
-            <path d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
-          </svg>
-          Profile
+          <FontAwesomeIcon icon={faUser} />
+          <button className="individualBtn">Profile</button>
         </li>
         <li>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <title>logout-variant</title>
-            <path d="M14.08,15.59L16.67,13H7V11H16.67L14.08,8.41L15.5,7L20.5,12L15.5,17L14.08,15.59M19,3A2,2 0 0,1 21,5V9.67L19,7.67V5H5V19H19V16.33L21,14.33V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5C3,3.89 3.89,3 5,3H19Z" />
-          </svg>
-          <button className='logoutClickBtn'onClick={ logoutSession }> Logout</button>
+          <FontAwesomeIcon icon={faRightFromBracket} />
+          <button className="individualBtn" onClick={logoutSession}>
+            {" "}
+            Logout
+          </button>
         </li>
       </ul>
     </div>
