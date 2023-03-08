1ac2c95e6a4a80d2d18606072a78a42fd9bf7931
garvsl <garvsehgal7@gmail.com>
2023-03-07 21:51:47 -0500

    have the evenlistener removed afterwards

--- client/src/components/logout.tsx
+++ client/src/components/logout.tsx
@@ -3,14 +3,22 @@ import ClickPopup from "./logoutPop";
 
 export default function LogoutButton() {
   const [clicked, setClicked] = useState<boolean>(false);
+
+  const clickHandler = (event: MouseEvent) => {
+    logoutRemoval(event, setClicked);
+  };
+
+  const logoutRemoval = (e: MouseEvent, setClicked: any) => {
+    const target = e.target as Element;
+    if (target && !target.closest(".logoutBtnCover")) {
+      setClicked(false);
+      document.removeEventListener("click", clickHandler);
+    }
+  };
+
   const click = () => {
     setClicked(clicked == false);
-    document.addEventListener("click", (e: MouseEvent) => {
-      const target = e.target as Element;
-      if (target && !target.closest(".logoutBtnCover")) {
-        setClicked(false);
-      }
-    });
+    document.addEventListener("click", clickHandler);
   };
   return (
     <div className="logoutBtnCover">
