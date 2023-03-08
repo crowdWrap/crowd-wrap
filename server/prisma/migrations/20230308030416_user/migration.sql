eabd13e6d2b26a7aa48a2dd43abdb9fec08fd189
garvsl <garvsehgal7@gmail.com>
2023-03-07 22:04:30 -0500

    delete old db and make new

--- server/prisma/migrations/20230308030416_user/migration.sql
+++ server/prisma/migrations/20230308030416_user/migration.sql
@@ -1,8 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "picture" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -17,6 +19,9 @@ CREATE TABLE "Post" (
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
