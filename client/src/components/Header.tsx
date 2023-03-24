4e03f51c7665e24982cb7f584c8701180bb1f30d
Vinh Pham <viph0009@gmail.com>
2023-03-24 15:40:32 -0700

    Update: fixed to the latest PR's comments

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,25 +1,6 @@
-/*import React from 'react';
-import {NavLink, Link} from 'react-router-dom'
-import '../assets/header.css';
-const Header = () => {
-    return(
-        <nav>
-            <div className='div-header'>
-                <div>
-                    <NavLink to='/'>
-                        <h1 className='logo'>CrowdWrap</h1>
-                    </NavLink> 
-                </div>
-            </div>
-        </nav>
-    )
-}
-
-export default Header*/ 
-
 import React from 'react';
 import '../assets/header.css';
-import {Link, useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import logoPrint from '../assets/crowdwrap-print.svg';
 const Header = () => (
   <div className="web-header">
--- client/src/pages/loginForm.tsx
+++ client/src/pages/loginForm.tsx
@@ -1,95 +1,94 @@
-import React from "react"; 
-import '../assets/form.css';
+import React from "react";
+import "../assets/form.css";
 import { useState } from "react";
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import Header from "../components/Header";
-import  "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-//fefefe background-color: #0efefe;
+async function fetchData(navigate: any) {
+  const response: Response = await fetch("/login", {
+    method: "GET",
+  });
+  // eslint-disable-next-line
+  const receivedData = await response.json();
 
-async function fetchData(navigate:any){
-    
-   const response: Response = await fetch('/login', {
-       method: 'GET'})
- 
-       const receivedData = await response.json();
- 
-       if (response.ok) {
-        } else {
-          navigate('/profile')
-        }      
+  if (response.ok) {
+  } else {
+    navigate("/profile");
+  }
 }
 
-export default function LoginForm(){
-   const navigate = useNavigate();
-  const [loginUsername, setLoginUsername] = useState<string>("")
-//   const [loginEmail, setLoginEmail] = useState<string>("")
-  const [loginPass, setLoginPassword] = useState<string>("")
+export default function LoginForm() {
+  const navigate = useNavigate();
+  const [loginUsername, setLoginUsername] = useState < string > ("");
+  const [loginPass, setLoginPassword] = useState < string > ("");
 
   useEffect(() => {
-   fetchData(navigate)
-}, []);
-  
-  async function handleSubmit (event: React.FormEvent<HTMLFormElement>) {
-   
-   event.preventDefault();
-      const data = JSON.stringify({
-         username: loginUsername,
-         // email: loginEmail,
-         password: loginPass
-      });
+    fetchData(navigate);
+    // eslint-disable-next-line
+  }, []);
 
-      const response: Response = await fetch('/login', {
-      method: 'POST',
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const data = JSON.stringify({
+      username: loginUsername,
+      password: loginPass,
+    });
+
+    const response: Response = await fetch("/login", {
+      method: "POST",
       headers: {
-         'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: data
-      })
+      body: data,
+    });
 
-      const receivedData = await response.json();
+    const receivedData = await response.json();
 
-      if (response.ok) {
-         alert(receivedData.message);
-         navigate('/profile');
-       } else {
-         alert(receivedData.message);
-       }
+    if (response.ok) {
+      alert(receivedData.message);
+      navigate("/profile");
+    } else {
+      alert(receivedData.message);
+    }
+  }
+  return (
+    <div className="wrapper loginForm">
+      <Header />
+      <div className="form-wrapper">
+        <div className="logintitle">Login</div>
+        <form onSubmit={handleSubmit} noValidate>
+          <div className="username">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              required
+              onChange={(e) => setLoginUsername(e.target.value)}
+            />
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+              onChange={(e) => setLoginPassword(e.target.value)}
+            />
+          </div>
 
-
-       
-   };    
-        return (
-          <div className='wrapper loginForm'>
-            <Header/>
-            <div className='form-wrapper'>
-               <div className="logintitle">Login</div>
-               <form onSubmit={handleSubmit} noValidate >
-                  <div className='username'>
-                     <label htmlFor="username">Username</label>
-                     <input type='text' name='username' placeholder="Username" required onChange= {e => setLoginUsername(e.target.value)}/>
-                  </div>
-                  {/* <div className='email'>
-                     <label htmlFor="email">Email</label>
-                     <input type='email' name='email' placeholder="Email" onChange={e => setLoginEmail(e.target.value)}/>
-                  </div> */}
-                  <div className='password'>
-                     <label htmlFor="password">Password</label>
-                     <input type='password' name='password' placeholder="Password" required onChange= {e => setLoginPassword(e.target.value)}/>
-                  </div>
-
-                  <div className='submit'>
-                     <button type='submit'>Login</button>
-                     <Link to="/register" className="linkstyle" style={{ fontSize: 12 }}>Don't Have an account yet? <span style={{ color: 'pink' }}>Register for free.</span></Link>
-                  </div>
-               </form>
-            </div>
-            {/*<div className="btnWrap">
-               <Link to="/register"><button className="signupBtn"> Don't Have an Account?</button></Link>
-               <Link to="/"><button className="loginBtn"> Home</button></Link>  style={{ textDecoration: 'none' }}
-               </div>*/}
-         </div>
-     );
+          <div className="submit">
+            <button type="submit">Login</button>
+            <Link to="/register" className="linkstyle" style={{ fontSize: 12 }}>
+              Don't Have an account yet?{" "}
+              <span style={{ color: "pink" }}>Register for free.</span>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
+
