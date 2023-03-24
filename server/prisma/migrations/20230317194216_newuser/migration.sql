0d843d144f9d673ed78db4c935b0f3c4087850c4
garvsl <garvsehgal7@gmail.com>
2023-03-24 16:17:56 -0400

    clear db and update migration

--- server/prisma/migrations/20230317194216_newuser/migration.sql
+++ server/prisma/migrations/20230317194216_newuser/migration.sql
@@ -1,4 +1,26 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "picture" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "_Friend" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
@@ -11,6 +33,12 @@ CREATE TABLE "_FriendRequests" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_Friend_AB_unique" ON "_Friend"("A", "B");
 
 -- CreateIndex
@@ -21,6 +49,9 @@ CREATE UNIQUE INDEX "_FriendRequests_AB_unique" ON "_FriendRequests"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_FriendRequests_B_index" ON "_FriendRequests"("B");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_Friend" ADD CONSTRAINT "_Friend_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
