a94e875dbd4a254b8076777507c1cae66788c521
Vinh Pham <viph0009@gmail.com>
2023-03-24 19:55:29 -0700

    Fixed: logout.tsx, Deleted: workspace file.

--- client/src/components/logout.tsx
+++ client/src/components/logout.tsx
@@ -3,14 +3,22 @@ import ClickPopup from "./logoutPop";
 
 export default function LogoutButton() {
   const [clicked, setClicked] = useState<boolean>(false);
+
+  const clickHandler = (event: MouseEvent) => {
+    logoutRemoval(event);
+  };
+
+  const logoutRemoval = (e: MouseEvent) => {
+    const target = e.target as Element;
+    if (target && !target.closest(".logoutBtnCover")) {
+      setClicked(false);
+      document.removeEventListener("click", clickHandler);
+    }
+  };
+
   const click = () => {
     setClicked(clicked == false);
-    document.addEventListener("click", (e: MouseEvent) => {
-      const target = e.target as Element;
-      if (target && !target.closest(".logoutBtnCover")) {
-        setClicked(false);
-      }
-    });
+    document.addEventListener("click", clickHandler);
   };
   return (
     <div className="logoutBtnCover">
