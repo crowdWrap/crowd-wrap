16e46e264eec929687e2b3c57c7f253379422c5f
garvsl <garvsehgal7@gmail.com>
2023-03-26 17:37:52 -0400

    add main page and search

--- client/src/components/friendList/friendsListCover.tsx
+++ client/src/components/friendList/friendsListCover.tsx
@@ -1,57 +1,114 @@
-import { useEffect, useState } from "react";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useRef, useState } from "react";
 
-export default function FriendsListCover() {
+async function fetchData() {
+  try {
+    const response = await fetch(`/friends`, {
+      method: "GET",
+    });
+    const result = await response.json();
+    return result;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+async function removeFriend(item: string, element: HTMLSpanElement) {
+  const parentElement = element.parentElement;
+  if (parentElement && parentElement.contains(element)) {
+    const response = await fetch(`/removeFriend?user_name=${item}`, {
+      method: "GET",
+    });
+    const result = await response.json();
+    if (result.success) {
+      parentElement.removeChild(element);
+    }
+    return result;
+  }
+}
+
+interface Account {
+  username: string;
+  profilePic: string;
+}
+
+export default function FriendsListCover(data: any) {
+  const [accounts, setAccounts] = useState<Account[]>([]);
+  const elements = useRef<(HTMLSpanElement | null)[]>([]);
+
+  const handleButtonClick = async (
+    item: string,
+    index: number,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
+    const element = elements.current[index];
+    if (element) {
+      await removeFriend(item, element);
+      setAccounts((prevAccounts) =>
+        prevAccounts.filter((account) => account.username !== item)
+      );
+    }
+  };
+
+  useEffect(() => {
+    let loaded = true;
+    (async () => {
+      if (loaded) {
+        setAccounts(await fetchData());
+      }
+    })();
+    return () => {
+      loaded = false;
+      setAccounts([]);
+    };
+  }, []);
+
+  //Why isnt it working!? When clicking on the button it should be null...so why is still displayed, does
+  //the component need to be updated?
+
   return (
     <div className="friendListCover">
-      <button className="friend">
-        <img
-          src="https://cdn.discordapp.com/attachments/463174949310562304/1075952796660146196/IMG_3462.png"
-          alt=""
-        />
-        <p>dog</p>
-      </button>
-      <button className="friend">
-        <img
-          src="https://cdn.discordapp.com/attachments/463174949310562304/1075952796660146196/IMG_3462.png"
-          alt=""
-        />
-        <p>dog</p>
-      </button>
-      <button className="friend">
-        <img
-          src="https://cdn.discordapp.com/attachments/463174949310562304/1075952796660146196/IMG_3462.png"
-          alt=""
-        />
-        <p>dog</p>
-      </button>
-      <button className="friend">
-        <img
-          src="https://cdn.discordapp.com/attachments/463174949310562304/1075952796660146196/IMG_3462.png"
-          alt=""
-        />
-        <p>dog</p>
-      </button>
-      <button className="friend">
-        <img
-          src="https://cdn.discordapp.com/attachments/463174949310562304/1075952796660146196/IMG_3462.png"
-          alt=""
-        />
-        <p>dog</p>
-      </button>
-      <button className="friend">
-        <img
-          src="https://cdn.discordapp.com/attachments/463174949310562304/1075952796660146196/IMG_3462.png"
-          alt=""
-        />
-        <p>dog</p>
-      </button>
-      <button className="friend">
-        <img
-          src="https://cdn.discordapp.com/attachments/463174949310562304/1075952796660146196/IMG_3462.png"
-          alt=""
-        />
-        <p>dog</p>
-      </button>
+      {accounts &&
+        !data.data &&
+        accounts.map((item, index) => (
+          <span
+            className="friend"
+            key={item.username}
+            ref={(currentElement) => (elements.current[index] = currentElement)}
+          >
+            <img src={item.profilePic} alt="" />
+            <p>{item.username}</p>
+            <button
+              onClick={(event) =>
+                handleButtonClick(item.username, index, event)
+              }
+              className="friendAddButton"
+            >
+              <FontAwesomeIcon icon={faX} />
+            </button>
+          </span>
+        ))}
+      {data.data &&
+        data.data.map((item: any, index: any) => (
+          <span
+            className="friend"
+            key={item.username}
+            ref={(currentElement) => (elements.current[index] = currentElement)}
+          >
+            <img src={item.profilePic} alt="" />
+            <p>{item.username}</p>
+            <button
+              onClick={(event) =>
+                handleButtonClick(item.username, index, event)
+              }
+              className="friendAddButton"
+            >
+              <FontAwesomeIcon icon={faX} />
+            </button>
+          </span>
+        ))}
     </div>
   );
 }
--- client/src/components/friendList/friendsListSearch.tsx
+++ client/src/components/friendList/friendsListSearch.tsx
@@ -1,10 +1,30 @@
 import { useEffect, useState } from "react";
 
-export default function FriendsListSearch() {
+async function fetchData(searchText: string) {
+  if (searchText.length >= 3) {
+    const response = await fetch(
+      `/friendsListSearch?user_search=${searchText}`,
+      {
+        method: "GET",
+      }
+    );
+    const result = await response.json();
+    return result;
+  } else {
+    return;
+  }
+}
+
+export default function FriendsListSearch({ updateData }: any) {
   const [searchText, setSearchText] = useState<string>("");
 
   useEffect(() => {
-    console.log(searchText);
+    async function fetchAndUpdate() {
+      const data = await fetchData(searchText);
+      await updateData(data);
+    }
+    fetchAndUpdate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchText]);
 
   return (
