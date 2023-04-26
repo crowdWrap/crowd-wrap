84b5d86b4ed16d7ff4d2b6f1f52eda50dc26de34
garvsl <garvsehgal7@gmail.com>
2023-04-26 01:31:33 -0400

    display participants in bottom and let them add

--- client/src/pages/events.tsx
+++ client/src/pages/events.tsx
@@ -2,6 +2,21 @@ import { useState, useEffect } from "react";
 import LogoutButton from "../components/logout";
 import FriendsList from "../components/friendList/friendslist";
 import CreateEventButton from "../components/createEvent/createEventButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+async function fetchData() {
+  try {
+    const response = await fetch(`/friends`, {
+      method: "GET",
+    });
+    const result = await response.json();
+
+    return result;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
 
 async function fetchEvents(setEvents: any) {
   const response: Response = await fetch("/events/retrieve", {
@@ -13,13 +28,103 @@ async function fetchEvents(setEvents: any) {
 }
 
 export default function Events() {
+  const [participantData, setParticipantData] = useState<any>([]);
   const [events, setEvents] = useState<any>([]);
+  const [displayFriends, setDisplayFriends] = useState(null);
+  const [accounts, setAccounts] = useState<any>([]);
+
+  useEffect(() => {
+    let loaded = true;
+    (async () => {
+      if (loaded) {
+        setAccounts(await fetchData());
+      }
+    })();
+    return () => {
+      loaded = false;
+      setAccounts([]);
+    };
+  }, []);
+
+  const handleAddParticipant = (e: any) => {
+    if (e.id !== displayFriends) {
+      setDisplayFriends(e.id);
+    } else {
+      setDisplayFriends(null);
+    }
+  };
+
+  const handleInvite = async (item: any, e: any) => {
+    const response: Response = await fetch(
+      `/events/participants/add?username=${await item.username}&eventId=${await e.id}`,
+      {
+        method: "GET",
+      }
+    );
+
+    setDisplayFriends(null);
+    // const data = await response.json();
+
+    // return data;
+  };
+
   useEffect(() => {
     fetchEvents(setEvents);
-    // console.log(events);
+
+    // console.log(events[0].participants[0]);
+    // (async () => {
+    //   console.log(await handleParticipants(events[0].participants[0].userId));
+    // })();
     //pass something up in order to refresh the page?
     //change someone across? that would beb reakign rules lets see
   }, []);
+
+  const handleMoney = (e: any) => {
+    const match = e.moneyGoal.match(/\d+/g);
+    if (match[0] && match[1]) {
+      return `${match[0]}-${match[1]}`;
+    } else {
+      return match[0];
+    }
+  };
+
+  const handleProgress = (e: any) => {
+    const match = e.moneyGoal.match(/\d+/g);
+    return match[0];
+  };
+
+  const handleParticipants = async (e: any) => {
+    const existingParticipant = participantData.find(
+      (p: any) => p.id === e.userId
+    );
+    if (existingParticipant) {
+      return existingParticipant.pic;
+    }
+
+    const response: Response = await fetch(
+      `/events/participants?participantId=${await e.userId}`,
+      {
+        method: "GET",
+      }
+    );
+
+    const data = await response.json();
+
+    const newParticipantData = [
+      ...participantData,
+      { pic: data.pic, id: e.userId, name: data.name },
+    ];
+    setParticipantData(newParticipantData);
+
+    return data.pic;
+  };
+
+  const handleDate = (e: any) => {
+    const dateObj = new Date(e.deadlineDate);
+    const options: object = { month: "long", day: "numeric", year: "numeric" };
+    const formattedDate = dateObj.toLocaleDateString("en-US", options);
+    return formattedDate;
+  };
   return (
     <div className="eventsPage">
       <nav className="loggedInNavbar">
@@ -32,19 +137,73 @@ export default function Events() {
       <div className="eventsCover">
         {events &&
           events.map((e: any) => (
-            <div className="event">
-              <div>{`Title: ${e.title}`}</div>
-              <div>{`Description: ${e.description}`}</div>
-              <div>{`Image: ${e.image}`}</div>
-              <div>{`CurrentFunds: ${e.Currentfunds}`}</div>
-              <div>{`InviteLink: ${e.inviteLink}`}</div>
-              <div>{`MoneyGoal: ${e.moneyGoal}`}</div>
-              <div>{`deadLineDate: ${
-                e.deadlineDate === null ? "No Date" : e.deadlineDate
+            <div className="event" key={e.id}>
+              <div className="eventDate">{`${
+                e.deadlineDate === null ? "No Deadline" : handleDate(e)
               }`}</div>
-              <div>{`deadlineTime: ${
+              <h1 className="eventTitle">{e.title}</h1>
+              <p className="eventDesc">{e.description}</p>
+              <div className="eventImg">{`${e.image}`}</div>
+              <div className="eventProgressWrap">
+                <h4>Progress:</h4>
+                <div className="eventProgressBar">
+                  <div
+                    style={{
+                      width: `${(e.Currentfunds / handleProgress(e)) * 100}%`,
+                    }}
+                    className="eventProgress"
+                  ></div>
+                </div>
+
+                <h4>${handleMoney(e) === "100" ? "100+" : handleMoney(e)}</h4>
+              </div>
+              <div className="eventParticipants">
+                {events &&
+                  e.participants.map((val: any) => {
+                    handleParticipants(val);
+                    // console.log(val);
+
+                    return participantData.map((newVal: any) => {
+                      if (newVal.id === val.userId) {
+                        return (
+                          <img key={newVal.id} src={`${newVal.pic}`} alt="" />
+                        );
+                      }
+                    });
+
+                    //what we are going to do now is to make sure that the participants display actually works, we do this by making the frontend first(bootleg)
+                    //when they press plus bring up a list of their friends that they can invite, have to make sure they arent already in the event
+                    //then make the route of adding them, and once they press invite refresh the component and add their picture
+                  })}
+                <FontAwesomeIcon
+                  onClick={() => handleAddParticipant(e)}
+                  className="addParticipant"
+                  icon={faPlus}
+                />
+                <div className="inviteeCover">
+                  {displayFriends === e.id &&
+                    accounts &&
+                    accounts.map((item: any, index: any) => (
+                      <div className="invitee" key={item.username}>
+                        <img alt="" src={item.profilePic} />
+                        <p>{item.username}</p>
+                        <FontAwesomeIcon
+                          className="addParticipant"
+                          onClick={() => handleInvite(item, e)}
+                          icon={faPlus}
+                          //filter those that have the same name as the one in the event participant
+                        />
+                      </div>
+                    ))}
+                </div>
+              </div>
+
+              {/* <div className="eventCurrentFunds">{`CurrentFunds: ${e.Currentfunds}`}</div> */}
+              {/* <div>{`InviteLink: ${e.inviteLink}`}</div> */}
+              {/* <div className="eventMoneyGoal">{`MoneyGoal: ${e.moneyGoal}`}</div> */}
+              {/* <div className="eventDate">{`deadlineTime: ${
                 e.deadlineTime === null ? "No Time" : e.deadlineTime
-              }`}</div>
+              }`}</div> */}
             </div>
           ))}
       </div>
