a4ad95e253cb9bc284ebafb4e7a4f68b99895e9a
garvsl <garvsehgal7@gmail.com>
2023-05-27 21:45:27 -0400

    fix issues with protected route

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import styles from "./header.module.css";
 import { Link } from "react-router-dom";
 import logoPrint from "../../assets/image_group/crowdwrap-print.svg";
-import { Button, Icon } from "@chakra-ui/react";
+import { Box, Button, Icon, Image, Progress } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import LogoutButton from "../logout/logout";
 import CreateEventButton from "../createEvent/createEventButton";
@@ -12,47 +12,63 @@ import { BsArrowThroughHeart } from "react-icons/bs";
 import "@fontsource/inter";
 
 export default function Header() {
-  const { authed } = useAuth();
-
-  useEffect(() => {
-    console.log("authed changed:", authed);
-  }, [authed]);
+  const { authed, loading } = useAuth();
 
   return (
     <div className={styles["header"]}>
       <Link to="/">
-        <img src={logoPrint} alt="CrowdWrap-Title" className={styles["img"]} />
+        <Image
+          src={logoPrint}
+          alt="CrowdWrap-Title"
+          className={styles["img"]}
+        />
       </Link>
-
-      {!authed && (
-        <div className={styles["sign-links"]}>
-          <Link to="/login" className={styles["sign-links__link"]}>
-            <Button
-              rightIcon={<Icon as={CgLogIn} />}
-              // colorScheme="linkedin"
-
-              variant="outline"
-            >
-              Login
-            </Button>
-          </Link>
-          <Link to="/register" className={styles["sign-links__link"]}>
-            <Button
-              colorScheme="pink"
-              // variant="outline"
-              rightIcon={<Icon as={BsArrowThroughHeart} />}
-            >
-              Signup
-            </Button>
-          </Link>
-        </div>
+      {loading && (
+        <>
+          <Progress
+            position={"absolute"}
+            top="0"
+            left="0"
+            width="100vw"
+            height={"5px"}
+            size="lg"
+            isIndeterminate
+          />
+        </>
       )}
-      {authed && (
-        <div className={styles["navbarCover"]}>
-          <CreateEventButton />
-          <FriendsList />
-          <LogoutButton />
-        </div>
+      {!loading && (
+        <>
+          {!authed && (
+            <Box className={styles["sign-links"]}>
+              <Link to="/login" className={styles["sign-links__link"]}>
+                <Button
+                  rightIcon={<Icon as={CgLogIn} />}
+                  // colorScheme="linkedin"
+
+                  variant="outline"
+                >
+                  Login
+                </Button>
+              </Link>
+              <Link to="/register" className={styles["sign-links__link"]}>
+                <Button
+                  colorScheme="pink"
+                  // variant="outline"
+                  rightIcon={<Icon as={BsArrowThroughHeart} />}
+                >
+                  Signup
+                </Button>
+              </Link>
+            </Box>
+          )}
+          {authed && (
+            <Box className={styles["navbarCover"]}>
+              <CreateEventButton />
+              <FriendsList />
+              <LogoutButton />
+            </Box>
+          )}
+        </>
       )}
     </div>
   );
--- client/src/protectRoute.tsx
+++ client/src/protectRoute.tsx
@@ -1,11 +1,29 @@
+import { Flex } from "@chakra-ui/react";
 import { useAuth } from "../src/hooks/authContext";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
+// const LoadingScreen = () => (
+//   <Flex
+//     width={"100vw"}
+//     height={"100vh"}
+//     justifyContent={"center"}
+//     alignItems={"center"}
+//   >
+//     <h1>Loading...</h1>
+//   </Flex>
+// );
+
 export default function RequiresAuth() {
-  const { authed } = useAuth();
+  const { authed, loading } = useAuth();
   let location = useLocation();
 
-  if (!authed) {
+  console.log("Auth loading: ", loading);
+
+  // if (loading) {
+  //   return <LoadingScreen />;
+  // }
+
+  if (!loading && !authed) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
   return <Outlet />;
