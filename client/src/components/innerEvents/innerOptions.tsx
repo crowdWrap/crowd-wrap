e19b8b09078568f50957d764da9ab41d86625a71
garvsl <garvsehgal7@gmail.com>
2023-06-18 18:43:06 -0400

    create component from options

---
+++ client/src/components/innerEvents/innerOptions.tsx
@@ -0,0 +1,80 @@
+import {
+  ButtonGroup,
+  Flex,
+  IconButton,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { AiOutlineSetting, AiFillMoneyCollect } from "react-icons/ai";
+import { BiShare } from "react-icons/bi";
+import PayPrompt from "./payPrompt";
+
+export default function InnerOptions({ onCopy, events }: any) {
+  const toast = useToast();
+  const [ownerPaymentType, setOwnerPaymentType] = useState("none");
+  const [loading, setLoading] = useState(false);
+  const { onOpen, isOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      if (events.ownerId) {
+        const response = await fetch(
+          `/payment/update?userId=${events.ownerId}`,
+          {
+            method: "GET",
+          }
+        );
+        const receivedData = await response.json();
+        setOwnerPaymentType(receivedData.paymentType);
+        setLoading(false);
+      }
+    })();
+  }, []);
+  return (
+    <>
+      <ButtonGroup>
+        <Flex gap="25px" flexDir="column">
+          {!loading && ownerPaymentType !== "none" && (
+            <IconButton
+              // pay with venmo
+              boxSize="100px"
+              fontSize={"50px"}
+              icon={<AiFillMoneyCollect />}
+              aria-label="setting"
+              onClick={onOpen}
+            />
+          )}
+          <IconButton
+            // will provide the option to change the name of the event, budget, etc
+            boxSize="100px"
+            fontSize={"50px"}
+            icon={<AiOutlineSetting />}
+            aria-label="setting"
+          />
+
+          <IconButton
+            aria-label="Share"
+            boxSize="100px"
+            fontSize={"50px"}
+            onClick={() => {
+              onCopy();
+              toast({
+                title: "Invite link copied to clipboard.",
+                status: "success",
+                duration: 2000,
+              });
+            }}
+            icon={<BiShare aria-label="ShareIcon" />}
+          />
+        </Flex>
+      </ButtonGroup>
+      <PayPrompt
+        isOpen={isOpen}
+        onClose={onClose}
+        email={ownerPaymentType.substring(7)}
+      />
+    </>
+  );
+}
