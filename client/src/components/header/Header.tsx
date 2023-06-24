1c1bbd427968105b9a8910b4a2d29b614a892461
garvsl <garvsehgal7@gmail.com>
2023-06-24 03:52:18 -0400

    remove header from login/signup

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -17,59 +17,39 @@ export default function Header() {
   useEffect(() => {}, [user, user.usernameSet]);
 
   return (
-    <div className={styles["header"]}>
-      <Link to="/">
-        <Image
-          src={logoPrint}
-          alt="CrowdWrap-Title"
-          className={styles["img"]}
-        />
-      </Link>
+    <>
       {loading && (
-        <>
-          <Progress
-            position={"absolute"}
-            top="0"
-            left="0"
-            width="100vw"
-            height={"5px"}
-            size="lg"
-            isIndeterminate
-          />
-        </>
+        <Progress
+          zIndex={99}
+          position={"absolute"}
+          top="0"
+          left="0"
+          backgroundColor="transparent"
+          width="100vw"
+          height={"5px"}
+          size="lg"
+          isIndeterminate
+        />
       )}
-      {!loading && (
-        <>
-          {!authed && (
-            <Box className={styles["sign-links"]}>
-              <Link to="/login" className={styles["sign-links__link"]}>
-                <Button rightIcon={<Icon as={CgLogIn} />} variant="outline">
-                  Login
-                </Button>
-              </Link>
-              <Link to="/register" className={styles["sign-links__link"]}>
-                <Button
-                  colorScheme="pink"
-                  rightIcon={<Icon as={BsArrowThroughHeart} />}
-                >
-                  Signup
-                </Button>
-              </Link>
-            </Box>
-          )}
-          {authed && (
+      {!loading && authed && (
+        <div className={styles["header"]}>
+          <Link to="/">
+            <Image
+              src={logoPrint}
+              alt="CrowdWrap-Title"
+              className={styles["img"]}
+            />
+          </Link>
+
+          <>
             <Box className={styles["navbarCover"]}>
-              {user && user.usernameSet && (
-                <>
-                  <CreateEventButton />
-                  <FriendsList />
-                </>
-              )}
+              <CreateEventButton />
+              <FriendsList />
               <LogoutButton />
             </Box>
-          )}
-        </>
+          </>
+        </div>
       )}
-    </div>
+    </>
   );
 }
