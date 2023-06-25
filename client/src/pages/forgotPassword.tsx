4098d31a2dab4e87df545c530d3309f8dd9c7168
garvsl <garvsehgal7@gmail.com>
2023-06-25 01:24:00 -0400

    integrate into form

--- client/src/pages/forgotPassword.tsx
+++ client/src/pages/forgotPassword.tsx
@@ -1,32 +1,29 @@
 import {
-  Box,
   Button,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   HStack,
-  Heading,
   Icon,
   Input,
   InputGroup,
   InputRightAddon,
+  InputRightElement,
   Link,
   PinInput,
   PinInputField,
   Step,
-  StepDescription,
   StepIcon,
   StepIndicator,
   StepNumber,
   StepSeparator,
   StepStatus,
-  StepTitle,
   Stepper,
-  Text,
   useSteps,
+  useToast,
 } from "@chakra-ui/react";
-import { Form, Link as ReactLink } from "react-router-dom";
-import backgroundImage from ".././assets/image_group/blue-pink-better-theme.png";
+import { Link as ReactLink, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import LoginAndSignupPage from "../components/loginAndSignup/LoginAndSignupPage";
 import { AiOutlineUser } from "react-icons/ai";
@@ -39,20 +36,101 @@ export default function ForgotPassword() {
   const [currentText, setCurrentText] = useState(
     "No worries, we'll send you reset instructions"
   );
-  const handleSubmit = () => {
-    if (enterPass) {
-      // make surre the pass is valid
-      // then redirect back to login
-      // with a toast
-    } else if (codePage && complete) {
-      // if(checkifitsvalid)
-      setEnterPass(true);
-      setCurrentText("Enter your new password");
-      setActiveStep(2);
+  const [invalid, setInvalid] = useState(false);
+  const [usernameEmail, setUsernameEmail] = useState("");
+  const [token, setToken] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState<string>("");
+  const [passwordTouched, setPasswordTouched] = useState<boolean>(false);
+  const [showNew, setShowNew] = useState(false);
+
+  const toast = useToast();
+
+  const validatePassword = (password: string) => {
+    if (password.length < 8 || password.length > 20) {
+      setPasswordError("Password must be between 8 and 20 characters");
+    } else if (password.includes(" ")) {
+      setPasswordError("Password must not contain spaces");
     } else {
-      setCodePage(true);
-      setCurrentText("Enter the code sent to the associated email");
-      setActiveStep(1);
+      setPasswordError("");
+    }
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    if (passwordTouched) validatePassword(e.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    if (enterPass) {
+      const data = JSON.stringify({
+        usernameEmail,
+        password,
+      });
+      const response = await fetch("/login/forgot/update", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      });
+      if (response.ok) {
+        setActiveStep(3);
+        toast({
+          title: "Password Changed!",
+          status: "success",
+          duration: 4000,
+        });
+        navigate("/login");
+      } else {
+        toast({
+          title: "Error.",
+          status: "error",
+          duration: 4000,
+        });
+      }
+    } else if (codePage && complete) {
+      const data = JSON.stringify({
+        usernameEmail,
+        token,
+      });
+      const response = await fetch("/login/forgot/check", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      });
+      const receivedData = await response.json();
+
+      if (receivedData.valid) {
+        setInvalid(false);
+        setEnterPass(true);
+        setCurrentText("Enter your new password");
+        setActiveStep(2);
+      } else {
+        setInvalid(true);
+      }
+    } else {
+      const data = JSON.stringify({
+        usernameEmail,
+      });
+      const response = await fetch("/login/forgot", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      });
+      if (response.ok) {
+        setCodePage(true);
+        setCurrentText(
+          "Enter the code sent to the associated email. It will expire after 5 minutes"
+        );
+        setActiveStep(1);
+      }
     }
   };
 
@@ -74,7 +152,11 @@ export default function ForgotPassword() {
           {!codePage ? (
             <FormControl variant="floating" id="usernameEmail" isRequired>
               <InputGroup>
-                <Input placeholder=" " />
+                <Input
+                  onChange={(e) => setUsernameEmail(e.target.value)}
+                  value={usernameEmail}
+                  placeholder=" "
+                />
                 <FormLabel bg="white">Username or Email</FormLabel>
                 <InputRightAddon userSelect="none">
                   <Icon as={AiOutlineUser} />@
@@ -86,8 +168,12 @@ export default function ForgotPassword() {
               <PinInput
                 size={"lg"}
                 autoFocus
-                onComplete={() => setComplete(true)}
+                onComplete={(e) => {
+                  setToken(e);
+                  setComplete(true);
+                }}
                 type="number"
+                isInvalid={invalid}
               >
                 <PinInputField />
                 <PinInputField />
@@ -101,7 +187,50 @@ export default function ForgotPassword() {
               </PinInput>
             </HStack>
           ) : (
-            <Input />
+            <>
+              <FormControl
+                isInvalid={passwordError === "" ? false : true}
+                variant="floating"
+                isRequired
+                id="password"
+              >
+                <InputGroup size="md">
+                  <Input
+                    // borderColor="white"
+                    placeholder=" "
+                    transition="0.25s ease-in-out"
+                    type={showNew ? "text" : "password"}
+                    onChange={(e) => handlePasswordChange(e)}
+                    onBlur={() => {
+                      setPasswordTouched(true);
+                      validatePassword(password);
+                    }}
+                    value={password}
+                    minLength={8}
+                    maxLength={15}
+                  />
+                  <FormLabel backgroundColor="pink">New Password</FormLabel>
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      marginTop="auto"
+                      marginBottom="auto"
+                      variant="outline"
+                      colorScheme="blue"
+                      h="1.75rem"
+                      size="sm"
+                      onClick={() => setShowNew(!showNew)}
+                    >
+                      {showNew ? "Hide" : "Show"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                {passwordTouched && (
+                  <FormErrorMessage position="absolute">
+                    {passwordError}
+                  </FormErrorMessage>
+                )}
+              </FormControl>
+            </>
           )}
 
           <Flex width="100%" flexDir="row" gap="8px">
